dafe6a2e2e7282e6a95cafbfecbb439dc2766d38
Michkel <mikkelmariegaard@gmail.com>
2017-04-26 16:25:55 +0200

    First website upload

---
+++ action_page.php
@@ -0,0 +1,18 @@
+<html>
+<title> Er _ en fag? </title>
+<link rel="stylesheet" type="text/css" href="/dashboard/erduenfag/styles.css" />
+<body>
+<?php
+$name = $_POST["name"];
+
+if ($name=="Rasmus") {
+	echo "<h1>Ja</h1>";
+}
+else {
+	echo "<h1>Nej</h1>";
+}
+
+
+?>
+</body>
+</html>
